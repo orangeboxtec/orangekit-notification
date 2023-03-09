f60509469bb3d5e08bbdac194f477858356d07dd
Diego Belli Mondego <diego@orangebox.technology>
2023-03-09 17:03:07 -0300

    migrating code (fix building)

--- src/native-test/kotlin/com/orangebox/kit/notification/NotificationIT.kt
+++ src/native-test/kotlin/com/orangebox/kit/notification/NotificationIT.kt
@@ -3,4 +3,4 @@ package com.orangebox.kit.notification
 import io.quarkus.test.junit.QuarkusIntegrationTest
 
 @QuarkusIntegrationTest
-class ExampleResourceIT : ExampleResourceTest()
+class NotificationIT : NotificationTest()
--- src/test/kotlin/com/orangebox/kit/notification/NotificationTest.kt
+++ src/test/kotlin/com/orangebox/kit/notification/NotificationTest.kt
@@ -1,12 +1,10 @@
 package com.orangebox.kit.notification
 
 import io.quarkus.test.junit.QuarkusTest
-import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class ExampleResourceTest {
+class NotificationTest {
 
     @Test
     fun testHelloEndpoint() {
